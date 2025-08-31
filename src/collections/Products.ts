9d8d45f081d6ce1f4cbb9c2bd08ee600ca4ad6cf
hanzladevofficial <hanzladev.official@gmail.com>
2025-08-31 17:01:20 +0500

    11: Products fetched based on category and subCategory

---
+++ src/collections/Products.ts
@@ -0,0 +1,42 @@
+import { CollectionConfig } from "payload";
+
+export const Products: CollectionConfig = {
+  slug: "products",
+  admin: {
+    useAsTitle: "name",
+  },
+  fields: [
+    { name: "name", type: "text", required: true },
+    {
+      name: "description",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "price",
+      type: "number",
+      required: true,
+      admin: {
+        description: "Price in pkr",
+      },
+    },
+    {
+      name: "category",
+      type: "relationship",
+      relationTo: "Categories",
+      hasMany: false,
+      required: true,
+    },
+    {
+      name: "image",
+      type: "upload",
+      relationTo: "media",
+    },
+    {
+      name: "refundPolicy",
+      type: "select",
+      options: ["30-day", "14-day", "7-day", "3-day", "1-day", "no-refund"],
+      defaultValue: "30-day",
+    },
+  ],
+};
