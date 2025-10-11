a8f68787327d96441c8f1e9ae50c15b69123ade6
hanzladevofficial <hanzladev.official@gmail.com>
2025-10-12 04:28:50 +0500

    fix: resolve INVALID_IMAGE_OPTIMIZE_REQUEST for blob images

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,16 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "localhost", // allow all images from localhost
+        hostname: "localhost",
       },
       {
         protocol: "https",
-        hostname: "**.vercel-storage.com",
+        hostname: "**.public.blob.vercel-storage.com",
       },
     ],
   },
